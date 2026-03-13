bd032ee77b3e2608c26fea57653c32283baf94dc
AnthonyPadovan <relvapadovan@gmail.com>
2026-03-13 16:14:33 -0300

    segundo commit

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
 <?php
 echo "Revisao de PHP";
+
+$nome = "Ana";
+$nota = 7;
+
+if ($nota >= 6) {
+    echo "Aprovado";
+} else {
+    echo "Reprovado";
+}
+
+for ($i = 1; $i <= 3; $i++) {
+    echo " Rodada $i";
+}
+$i = 0;
+
+while ($i <= 10000) {
+    echo "anthony $i";
+    $i++;
+}
